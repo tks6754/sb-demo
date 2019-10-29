889f003e84cf2fb71c080db385443a1b44aaed74
miao <tks6754@126.com>
2019-10-29 23:40:36 +0800

    update

--- src/main/java/com/miao/spring/demo/dao/UserMapper.java
+++ src/main/java/com/miao/spring/demo/dao/UserMapper.java
@@ -1,10 +1,17 @@
 package com.miao.spring.demo.dao;
 
 import com.miao.spring.demo.domain.User;
+import com.miao.spring.demo.domain.UserDTO;
+
+import java.util.List;
 
 public interface UserMapper {
 
     User getById(Integer id);
+
+    List<User> listUser(UserDTO userDTO);
+
+    int count(UserDTO userDTO);
 
 
 }
