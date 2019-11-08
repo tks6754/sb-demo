9b8a1fe69cf2f724bf51c5a7f868d9f81e60a33f
miao <qiqban@126.com>
2019-11-08 21:02:10 +0800

    quartz

---
+++ src/main/java/com/miao/spring/demo/job/PrintJob.java
@@ -0,0 +1,32 @@
+package com.miao.spring.demo.job;
+
+import com.miao.spring.demo.dao.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+public class PrintJob extends QuartzJobBean {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    Scheduler scheduler;
+
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("print -- print -- print --");
+
+    }
+
+
+
+}
